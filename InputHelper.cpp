caebad03e94b5a465b92cfc348178183437d727a
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2025-11-26 07:52:10 -0500

    InputHelper.cpp

---
+++ InputHelper.cpp
@@ -0,0 +1,25 @@
+#include "InputHelper.hpp"
+using namespace std;
+
+void InputHelper::leerTexto(const char* mensaje, char* buffer, int maxSize) {
+    cout << mensaje;
+    cin.getline(buffer, maxSize);
+}
+
+int InputHelper::leerOpcion(const char* mensaje, int min, int max) {
+    int opcion;
+
+    while (true) {
+        cout << mensaje;
+        cin >> opcion;
+
+        if (opcion >= min && opcion <= max) {
+            return opcion;
+        }
+
+        cout << "Opción inválida. Debe ser entre " << min << " y " << max << endl;
+
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+}
