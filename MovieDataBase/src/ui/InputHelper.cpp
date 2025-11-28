632babe55738cd34ec0ff84be8bf5626654d245e
jadevillavicencio <jade.villavicencio@ucsp.edu.pe>
2025-11-27 20:27:03 -0500

    Update InputHelper.cpp

--- MovieDataBase/src/ui/InputHelper.cpp
+++ MovieDataBase/src/ui/InputHelper.cpp
@@ -4,18 +4,31 @@ using namespace std;
 void InputHelper::leerTexto(const char* mensaje, char* buffer, int maxSize) {
     cout << mensaje;
     cin.getline(buffer, maxSize);
+
+  
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        buffer[0] = '\0';
+    }
 }
 
 int InputHelper::leerOpcion(const char* mensaje, int min, int max) {
     int opcion;
     while (true) {
         cout << mensaje;
-        cin >> opcion;
-        if (opcion >= min && opcion <= max) {
-            return opcion;
+
+        if (cin >> opcion) {
+            cin.ignore(10000, '\n'); 
+
+            if (opcion >= min && opcion <= max) {
+                return opcion;
+            }
+            cout << "Opcion invalida. Debe ser entre " << min << " y " << max << endl;
+        } else {
+            cout << "Entrada invalida. Debes ingresar un numero." << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
         }
-        cout << "Opción inválida. Debe ser entre " << min << " y " << max << endl;
-        cin.clear();
-        cin.ignore(1000, '\n');
     }
 }
